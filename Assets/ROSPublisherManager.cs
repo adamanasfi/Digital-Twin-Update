978b374b25e1cec9e6ceddfe03ab841af4c488ae
Adam Manasfi <manasfiadam4@gmail.com>
2025-01-28 18:27:07 +0200

    sending all assets to omniverse!

--- Assets/ROSPublisherManager.cs
+++ Assets/ROSPublisherManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
 using System.Security.Cryptography.Xml;
+using TMPro;
 using Unity.Robotics.ROSTCPConnector;
 using UnityEngine;
 
@@ -9,37 +11,53 @@ public class ROSPublisherManager : MonoBehaviour
     ROSConnection ros;
     float timeElapsed = 0.0f;
     int publishMessageFrequency = 1;
-    public GameObject ImageTarget, ModelTarget;
+    public GameObject ImageTarget;
     public static GameObject imageTarget;
-    public static GameObject modelTarget;
+    public GameObject vuforiaParent;
     RosMessageTypes.CustomInterfaces.WallMsg wall;
-    RosMessageTypes.CustomInterfaces.WallMsg husky;
+    RosMessageTypes.CustomInterfaces.WallMsg VRLAsset;
 
     void Start()
     {
         imageTarget = new GameObject("correctAxesImageTarget");
         ros = ROSConnection.GetOrCreateInstance();
         ros.RegisterPublisher<RosMessageTypes.CustomInterfaces.WallMsg>("/hololensWall");
-        ros.RegisterPublisher<RosMessageTypes.CustomInterfaces.WallMsg>("/husky");
+        ros.RegisterPublisher<RosMessageTypes.CustomInterfaces.WallMsg>("/assets");
         wall = new RosMessageTypes.CustomInterfaces.WallMsg();
-        husky = new RosMessageTypes.CustomInterfaces.WallMsg();
+        VRLAsset = new RosMessageTypes.CustomInterfaces.WallMsg();
     }
 
+    public void PublishActivatedModelTarget()
+    {
+        foreach (UnityEngine.Transform child in vuforiaParent.transform)
+        {
+            if (child.gameObject.activeSelf)
+            {
+                PublishAsset(child.gameObject);
+                break;
+            }
+        }
+    }
 
-    public void PublishHusky()
+    public void SaveImageTarget()
     {
         imageTarget.transform.position = ImageTarget.transform.position;
         imageTarget.transform.up = ImageTarget.transform.forward;
         imageTarget.transform.right = ImageTarget.transform.right;
         imageTarget.transform.forward = -ImageTarget.transform.up;
-        Vector3 globalPosition = ModelTarget.transform.position;
+    }
+
+
+    public void PublishAsset(GameObject asset)
+    {
+        Vector3 globalPosition = asset.transform.position;
         Vector3 localPosition = imageTarget.transform.InverseTransformPoint(globalPosition);
-        float y_angle = ModelTarget.transform.eulerAngles.y - imageTarget.transform.eulerAngles.y;
-        husky.name = "husky";
-        husky.position = new RosMessageTypes.Geometry.Vector3Msg(localPosition.x, localPosition.z, localPosition.y);
-        husky.rotation = new RosMessageTypes.Geometry.Vector3Msg(0, 0, -y_angle);
-        husky.scale = new RosMessageTypes.Geometry.Vector3Msg(1, 1, 1);
-        ros.Publish("/husky", husky);
+        float y_angle = asset.transform.eulerAngles.y - imageTarget.transform.eulerAngles.y;
+        VRLAsset.name = asset.name.ToString();
+        VRLAsset.position = new RosMessageTypes.Geometry.Vector3Msg(localPosition.x, localPosition.z, localPosition.y);
+        VRLAsset.rotation = new RosMessageTypes.Geometry.Vector3Msg(0, 0, -y_angle);
+        VRLAsset.scale = new RosMessageTypes.Geometry.Vector3Msg(1, 1, 1);
+        ros.Publish("/assets", VRLAsset);
     }
 
     public void PublishWalls()
