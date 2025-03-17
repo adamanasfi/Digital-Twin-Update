9afc56219e862b2e6eaf6d464675852bdda257d0
Adam Manasfi <manasfiadam4@gmail.com>
2025-03-17 15:36:02 +0200

    added ROS service to request STOD via UI

--- Assets/PrefabsManager.cs
+++ Assets/PrefabsManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class PrefabsManager : MonoBehaviour
 {
-    public GameObject HuskyPrefab, MonitorEchoPrefab, RedChairPrefab, KubokiPrefab, RedRobotPrefab;
+    public GameObject HuskyPrefab, MonitorEchoPrefab, RedChairPrefab, KubokiPrefab, RedRobotPrefab, GreenChairPrefab;
     public GameObject ToolTipPrefab;
     public static Dictionary<string, GameObject> prefabDictionary;
     public static GameObject STODParent;
@@ -16,7 +16,8 @@ public class PrefabsManager : MonoBehaviour
             { "MonitorEcho", MonitorEchoPrefab },
             { "RedChair", RedChairPrefab },
             { "Kuboki", KubokiPrefab },
-            { "RedRobot", RedRobotPrefab }
+            { "RedRobot", RedRobotPrefab },
+            {"GreenChair", GreenChairPrefab }
         };
         STODParent = new GameObject("STODParent");
         toolTipPrefab = ToolTipPrefab;
--- Assets/ROSClientManager.cs
+++ Assets/ROSClientManager.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Unity.Robotics.ROSTCPConnector;
+using RosMessageTypes.CustomedInterfaces;
 
 public class ROSClientManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    ROSConnection ros;
+
     void Start()
     {
-        
+        ros = ROSConnection.GetOrCreateInstance();
+        ros.RegisterRosService<RequestSTODRequest, RequestSTODResponse>("request_STOD");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CallSTODService()
     {
-        
+        RequestSTODRequest request = new RequestSTODRequest
+        {
+            agent_name = "hololens"
+        };
+
+        ros.SendServiceMessage<RequestSTODResponse>("request_STOD", request, STODResponseCallback);
     }
+
+    void STODResponseCallback(RequestSTODResponse response)
+    {
+        if (response.success)
+        {
+            Debug.Log("STOD request was successful!");
+        }
+        else
+        {
+            Debug.Log("STOD request failed.");
+        }
+    }
+
 }
