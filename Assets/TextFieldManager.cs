df21b82d42f44f42c52a53e2d6265e9f025e6885
Adam Manasfi <manasfiadam4@gmail.com>
2025-03-25 15:35:52 +0200

    added MRTK text field for robot ID

---
+++ Assets/TextFieldManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Microsoft.MixedReality.Toolkit.Experimental.UI;
+using UnityEngine.UIElements;
+
+public class TextFieldManager : MonoBehaviour
+{
+    public MRTKUGUIInputField ID;
+
+    void Start()
+    {
+        ID.onEndEdit.AddListener(HandleIDEdit);
+    }
+
+    void OnEnable()
+    {
+        Vector3 newPosition = Camera.main.transform.position + Camera.main.transform.forward * 1.0f;
+        ID.text = "";
+        ID.transform.position = newPosition;
+        ID.transform.forward = Camera.main.transform.forward;
+    }
+
+    void HandleIDEdit(string inputText)
+    {
+
+    }
+
+}
